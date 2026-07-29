0f66889370a4a71c709dec1b183682bfe871fe56
eliezeralmeidarandon <eliezer.almeida@terceiros.randon.com.br>
2026-07-29 18:12:47 -0300

    Fix typo in quality case for pps_fix

--- src/marnav/nmea/string.cpp
+++ src/marnav/nmea/string.cpp
@@ -170,7 +170,7 @@ std::string to_string(quality t)
 			return "1";
 		case quality::dgps_fix:
 			return "2";
-		case quality::pps_fixed:
+		case quality::pps_fix:
 			return "3";
 		case quality::rtk_fixed:
 			return "4";
